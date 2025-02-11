d5b19e11f7c6f37bbfa05775d806206eac929596
Prerna Saraogi <prernasaraogi22@gmail.com>
2025-02-11 09:02:28 +0530

    Added Editor & Sidebar components, and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
-import { useState } from 'react'
-
+import React, { useState, useEffect } from 'react';
+import Sidebar from './components/Sidebar';
+import Editor from './components/Editor';
+import Split from 'react-split';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [notes, setNotes] = useState([]);
+  const [currentNoteId, setCurrentNoteId] = useState("");
+
+  const currentNote = notes.find((note) => note.id === currentNoteId) || notes[0];
+
+  function createNewNote() {
+    const newNote = {
+      body: "# Type your markdown note's title here",
+    }
+  }
+
+  function updateNote() { }
+
+  function deleteNote() { }
 
   return (
-    <>
+    <main>
+      {notes.length > 0 ? (
+        <Split
+          sizes={[30, 70]}
+          direction="horizontal"
+          className="split">
+          <Sidebar
+            notes={sortedNotes}
+            currentNote={currentNote}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            deleteNote={deleteNote}
+          />
 
-    </>
+          <Editor
+            currentNote={currentNote}
+            updateNote={updateNote}
+          />
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
   )
 }
 
-export default App
+
